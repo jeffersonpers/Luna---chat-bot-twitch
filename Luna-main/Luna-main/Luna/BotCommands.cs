2864759606e6a1282f5e1d501eb2611b14b33309
Jefferson Carlos <jeffersonpers@gmail.com>
2022-05-25 08:17:36 -0300

    luna chat bot

---
+++ Luna-main/Luna-main/Luna/BotCommands.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Luna
+{
+    class BotCommands:Bot
+    {
+        public static String Commands(String command, String user_id, String user_name, String display_name)
+        {
+            switch (TextFormatting.CommandFormat(command))
+            {
+                case "join":
+                    if (db.BotJoin(user_id, user_name.ToLower()) == 1)
+                    {
+                        Bot.ChannelJoin(display_name);
+                        return $"Welcome to the group @{display_name}";
+                    }
+                    else
+                    {
+                        return "Error to join";
+                    }
+
+                case "leave":
+                    if (db.BotLeave(user_id) == 1)
+                    {
+                        Bot.ChannelLeave(display_name);
+                        return $"See you @{display_name}";
+                    }
+                    else
+                    {
+                        return "Error to leave";
+                    }
+
+                case "enableban":
+                    if (db.AutoBanEnable(user_id) == 1)
+                    {
+                        return "Auto ban is enabled in your channel";
+                    }
+                    else
+                    {
+                        return "Error to enable";
+                    }
+
+                case "disableban":
+                    if (db.AutoBanDisable(user_id) == 1)
+                    {
+                        return "Auto ban is disabled in your channel";
+                    }
+                    else
+                    {
+                        return "Error to disable";
+                    }
+
+                case "enabletranslation":
+                    if (Translator.AddChannelLanguage(user_name.ToLower(), user_id, command.Substring((command.IndexOf(' ') + 1), 5)) == 1)
+                    {
+                        return "Message translation is enabled in your channel";
+                    }
+                    else
+                    {
+                        return "Error to enable";
+                    }
+
+                case "disabletranslation":
+                    if (Translator.RemoveChannelLanguage(user_name.ToLower(), user_id) == 1)
+                    {
+                        return "Message translation is disabled in your channel";
+                    }
+                    else
+                    {
+                        return "Error to disable";
+                    }
+
+                default:
+                    return " ";
+            }
+        }
+    }
+}
---
+++ Luna-main/Luna-main/Luna/Translator.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+
+namespace Luna
+{
+    class Translator
+    {
+        static Database db = new();
+        static HttpClient httpclient = new();
+        static List<ChannelLanguage> channel_language = new(db.ChannelsWhereTranlationEnable());
+
+        public String Translate(String channel, String message)
+        {
+
+            try
+            {
+                String lang = SearchLanguage(channel);
+                if (lang == "")
+                {
+                    return "";
+                }
+                HttpResponseMessage response = httpclient.GetAsync($"https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={lang}&dt=t&q={TextFormatting.TranslationMessageWhithoutSpacesFormat(message)}").Result;
+                String translation = response.Content.ReadAsStringAsync().Result;
+                translation = translation.Substring(4, translation.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                if (!translation.Equals(message))
+                {
+                    return TextFormatting.TranslationMessageNormalFormat(translation);
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return "";
+        }
+
+        private String SearchLanguage(String channel)
+        {
+            foreach (ChannelLanguage channel_language in channel_language)
+            {
+                if (channel_language.GetChannel().Equals(channel))
+                {
+                    return channel_language.GetLanguage();
+                }
+            }
+            return "";
+        }
+
+        private static int SearchUser(String channel)
+        {
+            int i = 0;
+            foreach (ChannelLanguage channel_language in channel_language)
+            {
+                if (channel_language.GetChannel().Equals(channel))
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+
+        public static int AddChannelLanguage(String channel, String channel_id, String language)
+        {
+            ChannelLanguage temp = new(channel, language);
+            try
+            {
+                if (db.TranslationEnable(channel_id, language).Equals(1))
+                {
+                    channel_language.Add(temp);
+                    return 1;
+                }
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return 0;
+        }
+
+        public static int RemoveChannelLanguage(String channel, String channel_id)
+        {
+            try
+            {
+                if (db.TranlationDisable(channel_id).Equals(1))
+                {
+                    channel_language.RemoveAt(SearchUser(channel));
+                    return 1;
+                }
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return 0;
+        }
+    }
+
+    class ChannelLanguage
+    {
+        private String channel;
+        private String language;    
+
+        public ChannelLanguage(String channel, String language)
+        {
+            this.channel = channel;
+            this.language = language;
+        }
+
+        public void SetChannel(String channel)
+        {
+            this.channel=channel;
+        }
+
+        public String GetChannel()
+        {
+            return this.channel;
+        }
+
+        public void SetLanguage(String language)
+        {
+            this.language = language;
+        }
+
+        public String GetLanguage()
+        {
+            return this.language;
+        }
+    }
+}
